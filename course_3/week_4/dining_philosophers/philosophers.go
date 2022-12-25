b5784d9dd2d7ec18ebeb65ed6e7ab5960bef82cb
Yevhenii Orlov <orlovowork@gmail.com>
2022-12-25 03:58:05 +0200

    COURSE-3 Finished base implementation of dining philophers problem

--- course_3/week_4/dining_philosophers/philosophers.go
+++ course_3/week_4/dining_philosophers/philosophers.go
@@ -1,32 +1,16 @@
 package main
 
-//Implement the dining philosopher’s problem with the following constraints/modifications.
-
-//There should be 5 philosophers sharing chopsticks, with one chopstick between each adjacent pair of philosophers.
-
-//Each philosopher should eat only 3 times (not in an infinite loop as we did in lecture)
-
-//The philosophers pick up the chopsticks in any order, not lowest-numbered first (which we did in lecture).
-
-//In order to eat, a philosopher must get permission from a host which executes in its own goroutine.
-
-//The host allows no more than 2 philosophers to eat concurrently.
-
-//Each philosopher is numbered, 1 through 5.
-
-//When a philosopher starts eating (after it has obtained necessary locks) it prints “starting to eat <number>” on a line by itself, where <number> is the number of the philosopher.
-
-//When a philosopher finishes eating (before it has released its locks) it prints “finishing eating <number>” on a line by itself, where <number> is the number of the philosopher.
-
 import (
+	"golang.org/x/exp/slices"
 	"log"
 	"sync"
-	"time"
 )
 
-const MaxPhilos = 5
-const MaxMeals = 3
-const MaxEaters = 2
+const (
+	PhilosNum = 5
+	MealsNum  = 3
+	MaxEaters = 2
+)
 
 type chopS struct {
 	mut sync.Mutex
@@ -36,10 +20,24 @@ type host struct {
 	activeEaters []*philosopher
 }
 
-func (h *host) isPermitted(p *philosopher) bool {
-	// TODO:
-	// Add eater if there is enough space and is the eater is not currently eating
-	return false
+func (h *host) host(askChan chan *philosopher, answChan chan *philosopher, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < PhilosNum*MealsNum; i++ {
+		p := <-askChan
+		if len(h.activeEaters) < MaxEaters {
+			h.activeEaters = append(h.activeEaters, p)
+			log.Printf("Allowed philosopher %d to eat\n", p.id)
+			answChan <- p
+			fullPhilo := <-answChan
+			if slices.Contains(h.activeEaters, fullPhilo) {
+				idx := slices.Index(h.activeEaters, fullPhilo)
+				h.activeEaters = append(h.activeEaters[:idx], h.activeEaters[idx+1:]...)
+			}
+		} else {
+			log.Println("No available chopsticks for eating")
+			answChan <- nil
+		}
+	}
 }
 
 type philosopher struct {
@@ -48,56 +46,52 @@ type philosopher struct {
 	leftCs, rightCs *chopS
 }
 
-func (p *philosopher) eat(h *host, wg *sync.WaitGroup) {
+func (p *philosopher) eat(askChan chan *philosopher, answChan chan *philosopher, wg *sync.WaitGroup) {
 	defer wg.Done()
-	if h.isPermitted(p) {
-		for p.mealNum < MaxMeals {
+	for p.mealNum < MealsNum {
+		askChan <- p
+		if allowed := <-answChan; allowed != nil {
 			p.leftCs.mut.Lock()
 			p.rightCs.mut.Lock()
-
-			log.Printf("id: %d starting to eat", p.id)
-			time.Sleep(time.Second)
-			log.Printf("id: %d eating", p.id)
-			time.Sleep(time.Second)
-			log.Printf("id: %d finishing to eat", p.id)
-
+			log.Printf("Philosopher: %d starting to eat", p.id)
+			log.Printf("Philosopher: %d eating", p.id)
+			log.Printf("Philosopher: %d finishing to eat", p.id)
 			p.mealNum += 1
 			p.rightCs.mut.Unlock()
 			p.leftCs.mut.Unlock()
+			answChan <- p
 		}
 	}
 }
 
 func main() {
-	cSticks := make([]*chopS, MaxPhilos)
-	philos := make([]*philosopher, MaxPhilos)
+	cSticks := make([]*chopS, PhilosNum)
+	philos := make([]*philosopher, PhilosNum)
+
+	askChan := make(chan *philosopher)
+	answChan := make(chan *philosopher)
+
+	h := host{activeEaters: make([]*philosopher, 0)}
+
 	var wg sync.WaitGroup
 
-	for i := 0; i < MaxPhilos; i++ {
+	for i := 0; i < PhilosNum; i++ {
 		cSticks[i] = new(chopS)
 	}
 
-	for i := 0; i < MaxPhilos-1; i++ {
+	for i := 0; i < PhilosNum; i++ {
 		philos[i] = &philosopher{
 			id:      i + 1,
 			mealNum: 0,
 			leftCs:  cSticks[i],
-			rightCs: cSticks[i+1],
+			rightCs: cSticks[(i+1)%5],
 		}
 	}
 
-	philos[len(philos)-1] = &philosopher{
-		id:      MaxPhilos,
-		mealNum: 0,
-		leftCs:  cSticks[0],
-		rightCs: cSticks[MaxPhilos-1],
-	}
-
-	h := host{activeEaters: make([]*philosopher, MaxEaters)}
-
-	wg.Add(MaxPhilos)
-	for i := 0; i < MaxPhilos; i++ {
-		go philos[i].eat(&h, &wg)
+	wg.Add(PhilosNum + 1)
+	go h.host(askChan, answChan, &wg)
+	for i := 0; i < PhilosNum; i++ {
+		go philos[i].eat(askChan, answChan, &wg)
 	}
 	wg.Wait()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module golang-specialisation
 
 go 1.19
 
-require golang.org/x/exp v0.0.0-20221211140036-ad323defaf05 // indirect
+require (
+	golang.org/x/exp v0.0.0-20221211140036-ad323defaf05 // indirect
+)
